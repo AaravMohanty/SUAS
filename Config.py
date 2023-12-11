209737bba8da52169b4153216ced78f43eadd787
AaravMohanty <68161280+AaravMohanty@users.noreply.github.com>
2023-12-11 15:24:03 -0500

    Config file

---
+++ Config.py
@@ -0,0 +1,37 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../configs/mask_rcnn/mask-rcnn_r50-caffe_fpn_ms-poly-1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1)))
+
+# Modify dataset related settings
+data_root = 'C:/Users/15712/PycharmProjects/UAV/'
+metainfo = {
+    'classes': ('watch', ),
+    'palette': [
+        (220, 20, 60),
+    ]
+}
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='traincoco.json',
+        data_prefix=dict(img='')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='traincoco.json',
+        data_prefix=dict(img='val/')))
+test_dataloader = val_dataloader
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=data_root + 'traincoco.json')
+test_evaluator = val_evaluator
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
