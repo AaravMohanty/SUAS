3d58a1405c4f8b541afb1b57a37d60b01d3be0c8
Ken Zhou <kendotzhou@gmail.com>
2024-03-04 15:16:52 -0500

    test code for sending images with main loop

---
+++ Payload/mainloop_image_test.py
@@ -0,0 +1,30 @@
+import time
+import util
+import asyncio
+import socket
+
+async def run():
+    gps_socket = util.initGPSSocket()
+    img_socket = util.initImageSocket()
+    print("gps and image socket initialized, connecting to pixhawk...")
+    drone =  await util.connectToPixhawk()
+    print("connected to pixhawk")
+    #util.connectToCamera("wlan0")
+    print("running pre flight checks...")
+    checks = await util.preFlightChecks(drone)
+    print("pre flight checks complete")
+    if not checks[0]: # whether we're connected to the camera's WiFi SSID
+        print("connecting to camera...")
+        util.connectToCamera("wlan0")
+    for id in range(0, 3):
+        gps_data = util.GPSData(id, 0, 0, 0, 0)
+        print("sending fake GPS coordinate...")
+        gps_socket.sendall(gps_data.into_socket_msg())
+        print("taking picture...")
+        gopro_img = util.getImage()
+        print("sending image...")
+        util.send_image(img_socket, gps_socket, gopro_img)
+        print("image sent. waiting 2 seconds...")
+        time.sleep(2)
+asyncio.run(run())
+
